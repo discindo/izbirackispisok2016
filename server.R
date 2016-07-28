5b1e9cbd8262b9ddebb162d6eeb61e250f6bcbad
teo <teofiln@gmail.com>
2016-07-28 15:27:38 -0500

    added some control for showing all data for macedonia added boxplots for regions and minicipalities

--- server.R
+++ server.R
@@ -2,14 +2,14 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(RColorBrewer)
+library(gridExtra)
 
 MUN <- read.csv("Voters.csv")
 MUN <- MUN[!is.null(MUN$MUN),]
 
 shinyServer(function(input, output) {
-  
-  # filter based on selected species
-  
+
+  # filter based on selected minicipality
   getData <- reactive({
     if (input$choose_by == "Општина") {
       D <- MUN %>% dplyr::filter(MUN==input$Municipality)
@@ -18,9 +18,19 @@ shinyServer(function(input, output) {
     }
     return(D)
   })
+    
+  # macedonia or municipalities
+  getScale <- reactive({
+    if (input$details == "Цела Македонија") {
+      D <- MUN
+    } else {
+      D <- getData()
+    }
+    return(D)
+  })
   
   makePlot <- reactive({
-    D <- getData()
+    D <- getScale()
     # draw a barplot 
     P <- ggplot(D, aes(x=AGE)) +
       geom_histogram(bins=30, fill="firebrick1", colour="white") +
@@ -32,28 +42,67 @@ shinyServer(function(input, output) {
     return(P)
   })
   
-  # render the plot  
-  output$barPlot <- renderPlot({
-    P <- makePlot()
-    D <- getData()
+  makeBox <- reactive({
+    D <- getScale()
+    if (length(unique(D$REG)) > 1 && length(unique(D$MUN)) > 1) { 
+      P <- ggplot(D, aes(y=AGE, x=REG, group=REG, fill=REG)) +
+        geom_boxplot() + 
+        scale_fill_brewer("Регион",
+                          breaks=levels(D$REG),
+                          labels=levels(D$REG),
+                          palette = "Reds")
+      } else {
+      P <- ggplot(D, aes(y=AGE, x=MUN, group=MUN, fill=MUN)) +
+        geom_boxplot() +
+        scale_fill_brewer("Општина", 
+                        breaks=levels(D$MUN),
+                        labels=levels(D$MUN),
+                        palette = "Reds")
+    }
+    
+    P <- P +
+      theme_bw() +
+      xlab("") +
+      ylab("Возраст на спорни гласачи") +
+      scale_y_continuous(limits=c(18,100), breaks=seq(18, 100, 4)) +
+      theme(axis.text.x=element_text(angle=45,hjust = 1, vjust = 1),
+            legend.position="none") 
+    
+    return(P)
+  })
+  
+
+  # make titles
+  plotTitle <- reactive({
+    D <- getScale()
     Mun <- unique(D$MUN)
     Reg <- unique(D$REG)
     
-    if (length(unique(D$MUN)) > 1) {
-      P +# facet_grid(MUN ~ ., scales="free") + 
-        ggtitle(paste("Број на спорни гласачи возраст во", Reg, "регион", sep= " "))
+    if (length(unique(D$REG)) > 1 && length(unique(D$MUN)) > 1) {
+      P + ggtitle(paste("Број на спорни гласачи возраст во Македонија", sep= " "))
+    } else if (length(unique(D$MUN)) > 1) {
+      P + ggtitle(paste("Број на спорни гласачи возраст во", Reg, "регион", sep= " "))
     } else {
       P + ggtitle(paste("Број на спорни гласачи по возраст во општина", Mun, sep= " "))
     }
+  return(P)    
+  })
+
+# render the plot    
+  output$barPlot <- renderPlot({
+    P <- makePlot()
+    P1 <- makeBox()
+    
+    grid.arrange(P, P1, ncol=2)
   })
   
   # render a data summary
   output$Table <- renderDataTable({
-    D <- getData()[,2:5]
+    D <- getScale()[,2:5]
     D <- data.frame(D$YoB, D$AGE, D$REG, D$MUN)
     colnames(D) <- c("Година на раѓање", "Возраст", "Регион", "Општина")
     return(D)
-  },)
+  }, options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
   
   
 })
--- ui.R
+++ ui.R
@@ -11,33 +11,38 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(width = 4,
-                 radioButtons(inputId="choose_by",
-                              label="Одбери според:",
-                              choices=list("Регион", "Општина"),
-                              selected="Општина"),
-                 conditionalPanel(condition = "input.choose_by == 'Општина'",
-                                  selectInput(inputId="Municipality", 
-                                              label="Одбери Општина",
-                                              choices=levels(MUN$MUN))),
-                 conditionalPanel(condition = "input.choose_by == 'Регион'",
-                                  selectInput(inputId="Region", 
-                                              label="Одбери Регион",
-                                              choices=levels(MUN$REG),
-                                              selected="Скопски")),
+                 radioButtons(inputId="details",
+                              label="",
+                              choices=list("Цела Македонија","Региони и Општини"),
+                              selected="Цела Македонија"),
+                 conditionalPanel(condition = "input.details == 'Региони и Општини'",
+                   radioButtons(inputId="choose_by",
+                                label="Одбери според:",
+                                choices=list("Регион", "Општина"),
+                                selected="Општина"),
+                   conditionalPanel(condition = "input.choose_by == 'Општина'",
+                                    selectInput(inputId="Municipality", 
+                                                label="Одбери Општина",
+                                                choices=levels(MUN$MUN))),
+                   conditionalPanel(condition = "input.choose_by == 'Регион'",
+                                    selectInput(inputId="Region", 
+                                                label="Одбери Регион",
+                                                choices=levels(MUN$REG),
+                                                selected="Скопски"))
+                   ),
                  wellPanel(style = "background-color: #ffffff;",
                            h4("About"),
                            tags$div("Визуелизација на спорните гласачи од", tags$a(href="http://www.sec.mk/", "списокот објавен од страна на Државната изборна комисија во јули 2016."),
                                     "Податоците се групирани по општини и по региони. Град Скопје е извдоен посебно од Скопскиот регион.
                                     Оригиналните податоци се достапни", tags$a(href="https://drive.google.com/file/d/0B8ZpCwro9h-zcmtheXlEOGRsaHc/view", 'тука.'))
-                 )
+                    )
                  ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(width = 6,
+        
+        # Show a plot of the generated distribution
+    mainPanel(width = 8,
       plotOutput("barPlot"),
       tags$br(),
       dataTableOutput("Table")
     )
-    
-)
+  )
 ))
