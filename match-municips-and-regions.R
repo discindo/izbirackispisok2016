355dcba38c161ea96134c312892bac6b953bcca9
Teofil <teofiln@gmail.com>
2016-07-29 07:49:12 -0500

    fixed some name mismatches between tables of regions and voters.
    still some issues as 149 voters have no match for municipality

--- match-municips-and-regions.R
+++ match-municips-and-regions.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 A <- read.csv("mun1.csv",header=TRUE)
 B <- read.csv("regioni-opstini.csv", header=TRUE)
 colnames(B) <- c("REG", "MUN")
@@ -9,6 +11,9 @@ nrow(B)
 
 head(A)
 head(B)
+
+unique(A$MUN) %>% sort
+unique(B$MUN) %>% sort()
 
 C <- inner_join(A, B, by="MUN") 
 C$AGE <- 2016-C$YoB
